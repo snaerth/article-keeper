bd10b7f6868bad70da406d6e0c1a838c835b1d57
Snaer Seljan Thoroddsson <snaerth@gmail.com>
2017-08-05 18:10:01 +0000

    Added new debugger es6 package for es6 and fixed saveUser func

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -17,9 +17,9 @@ export function tokenForUser(user) {
   return jwt.encode(
     {
       sub: user.id,
-      iat: timestamp,
+      iat: timestamp
     },
-    process.env.JWT_SECRET,
+    process.env.JWT_SECRET
   );
 }
 
@@ -35,7 +35,7 @@ export async function findUserByEmail(email) {
     try {
       // See if user with given email exists
       const user = await User.findOne({
-        email,
+        email
       });
 
       // If a user does exist, return error
@@ -60,7 +60,7 @@ export async function findUserByEmail(email) {
  * @author Snær Seljan Þóroddsson
  */
 export function saveUser(user) {
-  return Promise(async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     if (user.constructor.name === 'model') {
       try {
         // Save user to database
@@ -74,7 +74,7 @@ export function saveUser(user) {
     } else {
       log.error(
         { err: new Error('Object is not a mongoose object') },
-        'Object is not a mongoose object',
+        'Object is not a mongoose object'
       );
 
       return reject('Object is not a mongoose object');
@@ -125,7 +125,7 @@ export async function uploadUserImage(req, res) {
       // Send response object with user token and user information
       return res.status(200).send({
         token: tokenForUser(updatedUser),
-        ...newUser,
+        ...newUser
       });
     } catch (err) {
       log.error({ req, res, err }, 'Error uploading user image');
@@ -167,7 +167,7 @@ export function checkUserByEmail(email) {
     try {
       // See if user with given email exists
       const user = await User.findOne({
-        email,
+        email
       });
       // If a user does exist, return error
       if (user) {
@@ -203,7 +203,7 @@ export async function updateUser(req, res) {
     newPassword,
     name,
     dateOfBirth,
-    phone,
+    phone
   });
 
   if (!error) {
@@ -236,9 +236,9 @@ export async function updateUser(req, res) {
             // Send response object with user token and user information
             return res.status(200).json({
               token: tokenForUser(updatedUser),
-              ...newUser,
+              ...newUser
             });
-          }),
+          })
       );
     } catch (err) {
       log.error({ req, res, err }, 'Error updating user');
@@ -297,13 +297,13 @@ export async function updateUserPassword({ token, password }) {
       const user = User.findOne({
         resetPasswordToken: token,
         resetPasswordExpires: {
-          $gt: Date.now() - hourInSeconds,
-        },
+          $gt: Date.now() - hourInSeconds
+        }
       });
 
       if (!user) {
         return reject({
-          error: 'Password reset token is invalid or has expired.',
+          error: 'Password reset token is invalid or has expired.'
         });
       }
 
