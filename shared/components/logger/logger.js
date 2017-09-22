617e6a7505c03688332ef1152b32627ceef5af05
Snær Seljan Þóroddsson <snaersth@islandsbanki.is>
2017-09-22 15:51:24 +0000

    Fixed linting in project

--- shared/components/logger/logger.js
+++ shared/components/logger/logger.js
@@ -262,11 +262,11 @@ class Logger extends Component {
                   </div>
                   <div>
                     <DateRangePicker
-                      startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                      endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                      startDate={this.state.startDate}
+                      endDate={this.state.endDate}
                       onDatesChange={({ startDate, endDate }) =>
-                        this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-                      focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                        this.setState({ startDate, endDate })}
+                      focusedInput={this.state.focusedInput}
                       onFocusChange={(focusedInput) =>
                         this.setState({ focusedInput })}
                       isOutsideRange={() => false}
