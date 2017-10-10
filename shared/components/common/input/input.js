2b08aed7e027daedcbc6a8c435da7cf627c1ddd5
Snær Seljan Þóroddsson <snaersth@islandsbanki.is>
2017-10-10 13:50:37 +0000

    Updating input styles

--- shared/components/common/input/input.js
+++ shared/components/common/input/input.js
@@ -1,61 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import s from './input.scss';
 import ErrorText from '../errorText';
 
-const Input = ({
-  meta,
-  id,
-  label,
-  children,
-  input,
-  type,
-  placeholder,
-  autocomplete,
-  hidelabel,
-  required,
-}) => (
-  <div>
-    <span className={s.input}>
-      {!hidelabel ? (
-        <label className={s.inputLabel} htmlFor={id}>
-          <span className={s.inputLabelContent}>{label}</span>
-        </label>
-      ) : null}
-      <input
-        {...input}
-        type={type}
-        className={s.inputField}
-        id={id}
-        name={id}
-        placeholder={placeholder}
-        autoComplete={autocomplete || 'off'}
-        required={required ? 'required' : ''}
-      />
-      {children ? (
-        <span className={classnames(s.icon, hidelabel ? s.iconNoLable : '')}>
-          {children}
-        </span>
-      ) : null}
-    </span>
-    {meta && meta.error && meta.touched ? (
-      <ErrorText key={id} id={id} error={meta.error} />
-    ) : null}
-  </div>
-);
+class Input extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    input: PropTypes.object,
+    meta: PropTypes.object,
+    autocomplete: PropTypes.string,
+    children: PropTypes.element,
+    hidelabel: PropTypes.bool,
+    required: PropTypes.bool,
+  };
 
-Input.propTypes = {
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
-  type: PropTypes.string.isRequired,
-  input: PropTypes.object,
-  meta: PropTypes.object,
-  autocomplete: PropTypes.string,
-  children: PropTypes.element,
-  hidelabel: PropTypes.bool,
-  required: PropTypes.bool,
-};
+  state = {
+    active: false,
+  };
+
+  onFocusHandler = () => {
+    this.setState({
+      active: !this.state.active,
+    });
+  };
+
+  render() {
+    const {
+      meta,
+      id,
+      label,
+      children,
+      input,
+      type,
+      placeholder,
+      autocomplete,
+      hidelabel,
+      required,
+    } = this.props;
+    const { active } = this.state;
+
+    return (
+      <div>
+        <span
+          className={classnames(
+            s.input,
+            active || input.value ? s.active : '',
+            meta && meta.error ? s.error : '',
+          )}
+        >
+          <input
+            {...input}
+            type={type}
+            className={s.inputField}
+            id={id}
+            name={id}
+            placeholder={placeholder}
+            autoComplete={autocomplete || 'off'}
+            required={required ? 'required' : ''}
+            onFocus={this.onFocusHandler}
+            onBlur={this.onFocusHandler}
+          />
+          {!hidelabel ? (
+            <label className={s.inputLabel} htmlFor={id}>
+              <span className={s.inputLabelContent}>{label}</span>
+            </label>
+          ) : null}
+          {children ? (
+            <span
+              className={classnames(s.icon, hidelabel ? s.iconNoLable : '')}
+            >
+              {children}
+            </span>
+          ) : null}
+        </span>
+        {meta && meta.error && meta.touched ? (
+          <ErrorText key={id} id={id} error={meta.error} />
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default Input;
