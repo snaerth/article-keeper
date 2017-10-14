5c361550fdcf8efb8a59b408a00cf68bfa38cbb5
Snær Seljan Þóroddsson <snaerth@gmail.com>
2017-10-14 20:02:20 +0000

    Fixed db service

--- api/services/__tests__/dbService.spec.js
+++ api/services/__tests__/dbService.spec.js
@@ -3,6 +3,6 @@ import dbSortValues from '../dbService';
 describe('Run tests for db service', () => {
   test('Check sort values array', () => {
     const sortVals = dbSortValues();
-    expect(sortVals).toEqual([1, -1, 'asc', 'desc']);
+    expect(sortVals).toEqual([1, -1, '1', '-1', 'asc', 'desc']);
   });
 });
