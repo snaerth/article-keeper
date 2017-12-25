ce7c2f1873b825abebee1975e7d3818faecaad7a
Snær Seljan Þóroddsson <snaerth@gmail.com>
2017-12-25 14:25:11 +0000

    Updated packages.json

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -44,9 +44,7 @@ const cspConfig = {
 const cspExtensions = config('cspExtensions');
 Object.keys(cspExtensions).forEach((key) => {
   if (cspConfig.directives[key]) {
-    cspConfig.directives[key] = cspConfig.directives[key].concat(
-      cspExtensions[key],
-    );
+    cspConfig.directives[key] = cspConfig.directives[key].concat(cspExtensions[key]);
   } else {
     cspConfig.directives[key] = cspExtensions[key];
   }
@@ -56,9 +54,7 @@ if (process.env.BUILD_FLAG_IS_DEV === 'true') {
   // When in development mode we need to add our secondary express server that
   // is used to host our client bundle to our csp config.
   Object.keys(cspConfig.directives).forEach((directive) => {
-    cspConfig.directives[directive].push(
-      `${config('host')}:${config('clientDevServerPort')}`,
-    );
+    cspConfig.directives[directive].push(`${config('host')}:${config('clientDevServerPort')}`);
   });
 }
 
